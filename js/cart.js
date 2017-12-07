29539baf0fc88fe603ea764f6a4bed886add98d5
Bautista Vigier <bautistavigiermtg@gmail.com>
2017-12-07 17:57:19 -0300

    comment adjust

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,7 @@
+/**
+ * Objeto modelo de carrito.
+ * @type {{products: Array, add: Cart.add, remove: Cart.remove, clear: Cart.clear, update: Cart.update, total: number}}
+ */
 var Cart = {
 	products: [],
 	add: function () {},
@@ -7,7 +11,10 @@ var Cart = {
 	total: 0
 };
 
-
+/**
+ * Objeto modelo de Producto
+ * @type {{id: number, title: string, img: string, price: string, quantity: number}}
+ */
 var Product = {
 	id: 0,
 	title: '',
